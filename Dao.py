5ec922117a24cf756065f18bae71fa9885df554d
msclruiz <msc_lruiz@accitesz.com>
2021-11-27 01:37:00 -0600

    Update Dao.py

--- Dao.py
+++ Dao.py
@@ -4,6 +4,15 @@ db=SQLAlchemy()
 
 class table(db.Model):
     __tablename__='empleados'
-    idOpcion=db.empleados 
-
-
+    id_nomina=Column(INTEGER, primary_key=True)
+    nombre=Column(String(50), unique=True)
+    direccion=Column(String(50), nulleable=False)
+    telefono=Column(String(50), nulleable=False)
+    rfc=Column(String(13), nulleable=False)
+    curp=Column(String(18), nulleabl=False )
+    imss=Column(String(11), nulleable=False)
+    id_puesto=Column(INTEGER, nulleabke=False)
+    id_jornada=Column(INTEGER, nulleable=False)
+    edoEmpleado=Column(String(1), nulleable=False)
+    fechaingreso=Column(String(10), nullleable=False)
+    CtaBancaria=Column(String(18), nulleable=Fakse)
