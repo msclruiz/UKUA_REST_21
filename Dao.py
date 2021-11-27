1c0e550b221fe637c1fabfebfc5c95cffc08ce6f
msclruiz <msc_lruiz@accitesz.com>
2021-11-27 01:26:36 -0600

    Update Dao.py

--- Dao.py
+++ Dao.py
@@ -1,2 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, INTEGER, String
+db=SQLAlchemy()
+
+class table(db.Model):
+    __tablename__='empleados'
+    idOpcion=db.empleados 
+
 
