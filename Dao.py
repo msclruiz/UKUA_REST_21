651ba50bf10ce820a2499e9f96e5fdcc71305894
msclruiz <msc_lruiz@accitesz.com>
2021-12-01 00:02:22 -0600

    ajustes
    ajustes

--- Dao.py
+++ Dao.py
@@ -1,22 +1,51 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, INTEGER, String
+import json
+from flask import jsonify
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
-#CAMPOS de la tabla de EMPLEADOS
+
+# CAMPOS de la tabla de EMPLEADOS
 class empleados(db.Model):
-    __tablename__='empleados'
-    id_nomina=Column(INTEGER, primary_key=True)
-    nombre=Column(String(50), unique=True)
-    direccion=Column(String(50), nullable=False)
-    telefono=Column(String(50), nullable=False)
-    rfc=Column(String(13), nullable=False)
-    curp=Column(String(18), nullable=False )
-    imss=Column(String(11), nullable=False)
-    id_puesto=Column(INTEGER, nullable=False)
-    id_jornada=Column(INTEGER, nullable=False)
-    edoEmpleado=Column(String(1), default='A')
-    fechaingreso=Column(String(10), nullable=False)
-    CtaBancaria=Column(String(18), nullable=False)
+    __tablename__ = 'empleados'
+    id_nomina = Column(INTEGER, primary_key=True)
+    nombre = Column(String(50), unique=True)
+    direccion = Column(String(50), nullable=False)
+    telefono = Column(String(50), nullable=False)
+    rfc = Column(String(13), nullable=False)
+    curp = Column(String(18), nullable=False)
+    imss = Column(String(11), nullable=False)
+    id_puesto = Column(INTEGER, nullable=False)
+    id_jornada = Column(INTEGER, nullable=False)
+    edoEmpleado = Column(String(1), default='A')
+    fechaingreso = Column(String(10), nullable=False)
+    CtaBancaria = Column(String(18), nullable=False)
     def consultaGeneral(self):
-        return self.query.all()
+        dict_json = {"estatus": "", "mensaje": "", "empleados": []}
+        try:
+            dict_json['estatus'] = "ok"
+            lista = self.querey.filter(empleados.estatus=='A').all()
+            if len(lista)>0:
+                dict_json['mensaje'] = 'LISTADO DE EMPLEADOS'
+                dict_json['empleados'] = self.to_json_list(lista)
+            else:
+                dict_json['mensaje'] = "NO HAY EMPLEADOS REGISTRADOS"
+        except:
+            dict_json['estatus'] = 'Error'
+            dict_json['mensaje'] = 'ERROR AL EJECUTAR LA CONSULTA DE EMPLEADOS'
+        return json.dumps(dict_json)
+
+    def to_json_list(self, lista):
+        lista_empleados = []
+        for o in lista:
+            lista_empleados.append(self.to_json(o))
+        return lista_empleados
+
+    def to_json(self,o):
+        empleado = {"id_nomina": o.id_nomina, "nombre": o.nombre, "direccion": o.direccion, "telefono": o.telefono,
+                    "rfc": o.rfc, "curp": o.curp, "imss": o.imss, "id_puesto": o.id_puesto, "id_jornada": o.id_jornada,
+                    "edoEmpleado": o.edoEmpleado, "fechaingreso": o.fechaingreso, "ctaBancaria": o.ctaBancaria}
+        return empleado
+
+
--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, jsonify
 import json
-from Dao import empleados,db
+from Dao import empleados, db
 
+app = Flask(__name__)
 
-
-app=Flask(__name__)
-
-#CONFIGURACION DE LA BASE DE DATOS CON SQLALQUEMY
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://coordinadorRH:Hola.123@localhost:3306/ukuadb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
+# CONFIGURACION DE LA BASE DE DATOS CON SQLALQUEMY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345@localhost:3306/ukuadb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route('/')
 def inicio():
     return 'Escuchando el servicio REST de SISTEMA: Dispersion de Nomina'
 
-#@app.route('/empleados')
-#def empleados():
+
+# @app.route('/empleados')
+# def empleados():
 #    return 'Empleados'
 
 @app.route('/empleados/<int:id_nomina>')
@@ -25,53 +23,67 @@ def empleado(id_nomina):
     return 'Consultando empleado # ' + str(id_nomina)
 
 
-#SECCION PARA INICIALIZAR LOS SERVICIOS A UTILIZAR GET POST ETC
+# SECCION PARA INICIALIZAR LOS SERVICIOS A UTILIZAR GET POST ETC
 
-#METODO GET
-@app.route('/opciones/',methods=['GET'])
+# METODO GET
+@app.route('/opciones/', methods=['GET'])
 def opciones():
-    opciones={"estatus":"ok","mensaje":"LISTADO DE OPCIONES",
-              "opciones":[{"idOpcion":1,"nombre":"LEONARDO","descripcion":"conocido"}]}
+    opciones = {"estatus": "ok", "mensaje": "LISTADO DE OPCIONES",
+                "opciones": [{"idOpcion": 1, "nombre": "LEONARDO", "descripcion": "conocido"}]}
     return json.dumps(opciones)
 
-@app.route('/empleados/',methods=['GET'])
-def eempleados():
-    eempleados={"estatus":"ok","mensaje":"LISTADO DE OPCIONES",
-              "opciones":[{"idOpcion":1,"nombre":"LEONARDO","descripcion":"conocido"}]}
-    o=empleados()
-    lista=o.consultaGeneral()
-    for objeto in lista:
-        print(objeto.nombre)
-    return json.dumps(eempleados)
+1
+@app.route('/empleados/', methods=['GET'])
+def Empleados():
+    o = empleados()
+    lista = o.consultaGeneral()
+    return lista
 
-@app.route('/opciones/<int:id>',methods=['GET'])
+#   Empleados = {"estatus": "ok", "mensaje": "LISTADO DE OPCIONES",
+#                 "opciones": [{"id_nomina": 97420411, "nombre": "LEONARDO", "direccion": "conocido"}]}
+
+
+#    for objeto in lista:
+#        print(objeto.nombre)
+#    return json.dumps(Empleados)
+#     return lista
+
+
+
+@app.route('/opciones/<int:id>', methods=['GET'])
 def opcion(id):
-    opcion={"estatus":"ok","mensaje":"LISTADO DE OPCIONES",
-            "opciones":[{"idOpcion":id,"nombre":"LEONARDO","descripcion":"conocido"}]}
+    opcion = {"estatus": "ok", "mensaje": "LISTADO DE OPCIONES",
+              "opciones": [{"idOpcion": id, "nombre": "LEONARDO", "descripcion": "conocido"}]}
     return json.dumps(opcion)
 
 
-#METODO POST
+# METODO POST
 @app.route('/opciones/', methods=['POST'])
 def registroOpcion():
     opcion = request.get_json()
-    salida = {"estatus":"ok", "mensaje":"OPCION REGISTRADA CON EXITO"}
+    salida = {"estatus": "ok", "mensaje": "OPCION REGISTRADA CON EXITO"}
     return json.dumps(salida)
 
-#METODO PUT
+
+# METODO PUT
 @app.route('/opciones/', methods=['PUT'])
 def modificarOpcion():
-    opcion=request.get_json()
-    salida={"esetatus":"ok","mensaje":"OPCION MODIFICADA CON EXITO"}
+    opcion = request.get_json()
+    salida = {"esetatus": "ok", "mensaje": "OPCION MODIFICADA CON EXITO"}
     return json.dumps(salida)
 
-#METODO DELETE
+
+# METODO DELETE
 @app.route('/opciones/<int:id>', methods=['DELETE'])
 def eliminaOpcion(id):
-    salida={"estatus":"ok","mensaje":"OPCION # " + str(id) + " ELIMINADA CON EXITO"}
-    return  jsonify(salida)
+    salida = {"estatus": "ok", "mensaje": "OPCION # " + str(id) + " ELIMINADA CON EXITO"}
+    return jsonify(salida)
 
-if __name__=='__main__':
+
+
+
+
+
+if __name__ == '__main__':
     db.init_app(app)
     app.run(debug=True, host='0.0.0.0', port=5000)
-
